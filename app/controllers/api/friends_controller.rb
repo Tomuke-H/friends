fbbb684b0d292b431c46666936474ef1042731fe
Tom Hohl <tom.hohl@gmail.com>
2021-09-08 16:43:48 -0600

    backend works!

--- app/controllers/api/friends_controller.rb
+++ app/controllers/api/friends_controller.rb
@@ -1,2 +1,44 @@
 class Api::FriendsController < ApplicationController
+
+    before_action :set_friend, only: [:show, :update, :destroy]
+
+    def index
+        render json: Friend.all
+    end
+
+    def show
+        render json: @friend
+    end
+
+    def create
+        @friend = Friend.new(friends_params)
+        if(@friend.save)
+            render json: @friend
+        else
+            render json: @friend.errors.full_messages, status: 422
+        end
+    end
+
+    def update
+        if(@friend.update(friends_params))
+            render json: @friend
+        else
+            render json: @friend.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        render json: @movie.destroy
+    end
+
+    private
+
+    def friends_params
+        params.require(:friends).permit(:name, :age, :sign, :mental_illness)
+    end
+
+    def set_friend
+        @friend = Friend.find(params[:id])
+    end
+
 end
--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,2 +1,49 @@
 class Api::MoviesController < ApplicationController
+    before_action :set_friend
+    before_action :set_movie, only: [:show, :update, :destroy]
+
+    def index
+        render json: @friend.movies.all
+    end
+
+    def show
+        render json: @movie
+    end
+
+    def create
+        @movie = @friend.movie.new(movies_params)
+        if(@movie.save)
+            render json: @movie
+        else
+            render json: @movie.errors.full_messages, status: 422
+        end
+    end
+
+    def update
+        if(@movie.update(movies_params))
+            render json: @movie
+        else
+            render json: @movie.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        render json: @movie.destroy
+    end
+
+    private
+
+    def movies_params
+        params.require(:movies).permit(:title, :genre)
+    end
+
+    def set_friend
+        @friend = Friend.find(params[:friend_id])
+    end
+
+    def set_movie
+        @movie = @friend.movies.find(params[:id])
+    end
+
+
 end
