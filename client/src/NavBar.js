ea056c6e03392b1629e7a8d875bdad4687b94d91
Tom Hohl <tom.hohl@gmail.com>
2021-09-09 20:43:09 -0600

    navbar styling

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Menu, Button } from 'semantic-ui-react'
 
 const NavBar = () => {
     return (
-        <div>
-            <h1>NavBar</h1>
-            <Link to="/">Home</Link>
-            <Link to="/friends/">Friends</Link> 
-            <Link to="/friends/new/">New</Link> 
-        </div>
+        // <div>
+        //     <h1>NavBar</h1>
+        //     <Link to="/">Home</Link>
+        //     <Link to="/friends/">Friends</Link> 
+        //     <Link to="/friends/new/">New</Link> 
+        // </div>
+        <Menu>
+            <Menu.Item>
+                <Button>
+                    <Link to="/">Home</Link>
+                </Button>
+            </Menu.Item>
+        
+            <Menu.Item>
+                <Button>
+                    <Link to="/friends/">Friends</Link> 
+                </Button>
+            </Menu.Item>
+
+            <Menu.Item>
+                <Button>
+                    <Link to="/friends/new">New Friend</Link> 
+                </Button>
+            </Menu.Item>
+        </Menu>
     )
 }
 
