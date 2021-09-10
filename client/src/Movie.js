15e36074eeb1ab8059240d8e2d5a3e5457606a7c
Michaela Sivertsen <mjsivertsen@msn.com>
2021-09-09 20:43:59 -0600

    card stuff done on movie

--- client/src/Movie.js
+++ client/src/Movie.js
@@ -1,15 +1,35 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Button, Card, Image } from 'semantic-ui-react'
+
 
 const Movie = ({ movie, deleteMovie}) => {
     return (
         <div>
-            <h1>Movie title: {movie.title}</h1>
-            <h3>Genre: {movie.genre}</h3>
-            <Link to={`/friends/${movie.friend_id}/movies/${movie.id}/edit`}>Edit</Link>
-            <button onClick={() => deleteMovie(movie.id)}>Delete</button>
+            <Card style={{margin: "20px"}}>
+            <Card.Content>
+                {/* <Image
+                floated='right'
+                size='mini'
+                /> */}
+                <Card.Header >{movie.title}</Card.Header>
+                <Card.Meta>{movie.genre}</Card.Meta>
+                {/* <Card.Description>
+                Has a problem with {`${friend.mental_illness}`}
+                </Card.Description> */}
+            </Card.Content>
+            <Card.Content extra>
+                <div className='ui two buttons'>
+                <Button basic color='orange'>
+                    <Link style={{color:"teal"}} to={`/friends/${movie.friend_id}/movies/${movie.id}/edit`}>Edit Movie</Link>
+                </Button>
+                <Button basic color='red' onClick={() => deleteMovie(movie.id)}>DeleteMovie</Button>
+                </div>
+            </Card.Content>
+        </Card>
         </div>
     )
-}
+     };
+    
 
 export default Movie;
